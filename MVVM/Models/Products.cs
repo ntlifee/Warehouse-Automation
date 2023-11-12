c7f8c25da5c8200e5847a3e1254607c11959a91e
ntlifee <perfilov.mv@yandex.ru>
2023-11-12 17:40:46 +0300

    добавил слайдер и несколько классов

---
+++ MVVM/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseAutomation.MVVM.Models
+{
+    internal class Products
+    {
+        public string Type { get; set; }
+        public byte NumberPacks { get; set; }
+        public byte Volume { get; set; }
+        public uint Price { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
---
+++ MVVM/Models/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseAutomation.MVVM.Models
+{
+    internal class Settings
+    {
+        private int _numberTypesProducts;
+
+        public int NumberTypesProducts
+        {
+            get { return _numberTypesProducts; }
+            set { _numberTypesProducts = value; }
+        }
+
+        private int _numberStores;
+
+        public int NumberStores
+        {
+            get { return _numberStores; }
+            set { _numberStores = value; }
+        }
+
+        private int _numberSimulationDays;
+
+        public int NumberSimulationDays
+        {
+            get { return _numberSimulationDays; }
+            set { _numberSimulationDays = value; }
+        }
+
+        private int _storageCapacityProduct;
+
+        public int StorageCapacityProduct
+        {
+            get { return _storageCapacityProduct; }
+            set { _storageCapacityProduct = value; }
+        }
+    }
+}
